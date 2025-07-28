3aafae1f133e52ac2606e4d75690664866a18720
emitskevich <evg.mitskevich@gmail.com>
2025-07-28 22:08:39 +0300

    Use ThreadingHTTPServer

--- computer-use-demo/image/http_server.py
+++ computer-use-demo/image/http_server.py
@@ -1,16 +1,15 @@
 import os
-import socket
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from http.server import SimpleHTTPRequestHandler, ThreadingHTTPServer
 
 
-class HTTPServerV6(HTTPServer):
-    address_family = socket.AF_INET6
+# class HTTPServerV6(HTTPServer):
+#     address_family = socket.AF_INET6
 
 
 def run_server():
     os.chdir(os.path.dirname(__file__) + "/static_content")
     server_address = ("::", 8080)
-    httpd = HTTPServerV6(server_address, SimpleHTTPRequestHandler)
+    httpd = ThreadingHTTPServer(server_address, SimpleHTTPRequestHandler)
     print("Starting HTTP server on port 8080...")  # noqa: T201
     httpd.serve_forever()
 
